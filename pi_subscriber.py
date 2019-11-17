20e5bc2be7f1201cb579d3f6c9b2ad854fb2c0d9
Nathaniel Johnston <43395364+nathaniel-johnston@users.noreply.github.com>
2019-11-16 21:48:31 -0500

    Update pi_subscriber.py

--- pi_subscriber.py
+++ pi_subscriber.py
@@ -1,21 +1,24 @@
 import paho.mqtt.client as mqtt
 import time
 import serial
+import json
 
 ser = serial.Serial('/dev/cu.usbmodem14101', 9600)
 time.sleep(2)
 
 def on_message(client, userdata, message):
-  ser.write(message.payload.encode("ascii"))
-  #ser.write(b'1')
+  jsonPayload = json.loads(message.payload.decode("utf-8"))
+  
+  numPills = jsonPayload["numPills"]
+  pillId = jsonPayload["pillId"] + 1
+
+  ser.write(str(numPills*pillId).encode('ascii'))
 
 broker = "broker.hivemq.com"
-client = mqtt.Client("P1")
-client.on_message=on_message
+client = mqtt.Client()
+client.on_message = on_message
 client.connect(broker)
 
-client.loop_start() #start the loop
 client.subscribe("pills")
 
-while True:
-  time.sleep(1)
+client.loop_forever() #start the loop
